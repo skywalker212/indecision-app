74b90e9ba5acb71d795fc5a00ee932a11bfacc2b
Akash Gajjar <akashgajjar8@gmail.com>
2018-05-24 03:48:12 +0530

    styled modal

--- src/components/option-modal.js
+++ src/components/option-modal.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Modal from 'react-modal';
 
 const OptionModal = (prop) => (
-    <Modal isOpen={!!prop.selectedOption} contentLabel="Selected Option" onRequestClose={prop.clearSelected}>
+    <Modal isOpen={!!prop.selectedOption} contentLabel="Selected Option" onRequestClose={prop.clearSelected} closeTimeoutMS={200} className='modal'>
         <h3>Selected Option</h3>
         {prop.selectedOption && <p>{prop.selectedOption}</p>}
         <button onClick={prop.clearSelected}>OK</button>
